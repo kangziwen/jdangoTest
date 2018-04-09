fac267486b10bb111c74cdff9cac95772e863584
baobao.kang <baobao.kang@godinsec.com>
2018-04-09 16:20:52 +0800

    三种方式上传文件

--- djangoTest/app02/urls.py
+++ djangoTest/app02/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from app02 import views
 urlpatterns = [
     url(r'^login/',views.login),
-
+    url(r'^upload/', views.upload),
+    url(r'^upload_file/', views.upload_file),
 
 ]
--- djangoTest/app02/views.py
+++ djangoTest/app02/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse,render,redirect
 def login(request):
     return HttpResponse('login')
+
+def upload(request):
+    return render(request,'upload.html')
+from django.views.decorators.csrf import csrf_protect,csrf_exempt
+# 去除csrf
+@csrf_exempt
+def upload_file(request):
+
+    username = request.GET.get('username')
+    fafafa = request.FILES.get('fafafa')
+    print('fafafa : ',fafafa)
+    import os
+    img_path = os.path.join('static/images',fafafa.name)
+    with open(img_path,'wb') as f:
+        for item in fafafa.chunks():
+            f.write(item)
+    ret = {'code':True,'data':img_path}
+    import json
+    return HttpResponse(json.dumps(ret))
+
